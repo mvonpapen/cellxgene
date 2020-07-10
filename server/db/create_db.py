b9e4a915ff9ddf4634d72aabb75ec1d2d39ec8d4
Madison Dunitz <dunitzm@gmail.com>
2020-07-10 09:48:43 -0700

    save point

--- server/db/create_db.py
+++ server/db/create_db.py
@@ -8,13 +8,14 @@ import sys
 from sqlalchemy import create_engine
 
 from server.db.cellxgene_orm import Base
+from server.db.db_config import CellxGeneDbConfig
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
 
 def create_db():
-    engine = create_engine('postgresql://postgres:test_pw@localhost:5432')
+    engine = create_engine(CellxGeneDbConfig().database_uri())
     print("Dropping tables")
     Base.metadata.drop_all(engine)
     print("Recreating tables")
