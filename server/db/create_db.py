9f641ebcd066564a87ae7aad400784244f8920d9
Madison Dunitz <dunitzm@gmail.com>
2020-07-10 15:51:26 -0700

    fix config

--- server/db/create_db.py
+++ server/db/create_db.py
@@ -2,21 +2,13 @@
 Drops and recreates all tables according to cellxgene_orm.py
 """
 
-import os
-import sys
-
-from flask import current_app
 from sqlalchemy import create_engine
 
 from server.db.cellxgene_orm import Base
 
-# pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # noqa
-# sys.path.insert(0, pkg_root)  # noqa
 
-config = current_app.app_config
-
-def create_db():
-    engine = create_engine(config.relational_db__database_uri)
+def create_db(database_uri):
+    engine = create_engine(database_uri)
     print("Dropping tables")
     Base.metadata.drop_all(engine)
     print("Recreating tables")
